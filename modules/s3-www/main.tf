3266c11e6ce9069a33ee03cbd9fce1e9af33b184
stuart <stuart.dale@gmail.com>
2019-09-28 14:11:13 +0100

    module

---
+++ modules/s3-www/main.tf
@@ -0,0 +1,30 @@
+#s3 to host a static site
+resource "aws_s3_bucket" "www" {
+  bucket = "${var.bucketName}"
+  acl       = "public-read"
+ tags = "${merge(var.commonTags,map("role", "s3-wwww"))}"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+#bucket policy
+resource "aws_s3_bucket_policy" "www_policy" {
+  bucket = "${aws_s3_bucket.www.id}"
+  policy = "${data.aws_iam_policy_document.www_s3_policyDoc.json}"
+}
+#iamPolicReadGet
+data "aws_iam_policy_document" "www_s3_policyDoc" {
+  statement {
+    sid    = "publicReadForGetBucketObjects"
+    effect = "Allow"
+
+    principals {
+      type        = "aws"
+      identifiers = ["*"]
+    }
+
+    actions   = ["s3:GetObjects"]
+    resources = ["${aws_s3_bucket.www.arn}/*"]
+  }
+}
---
+++ modules/s3-www/variables.tf
@@ -0,0 +1,6 @@
+variable "bucketName" {}
+variable "commonTags" {
+    type = "map"
+}
+
+variable "envName" {}
